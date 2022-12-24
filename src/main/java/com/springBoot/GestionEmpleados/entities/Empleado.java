2e07d7590aa5fbdfc3935ff761739629ecf02422
MatiBalbiDev <matiasbalbi96@gmail.com>
2022-12-24 15:28:47 -0300

    add entity empleado

---
+++ src/main/java/com/springBoot/GestionEmpleados/entities/Empleado.java
@@ -0,0 +1,99 @@
+package com.springBoot.GestionEmpleados.entities;
+
+import java.util.Date;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table( name = "Empleados")
+public class Empleado {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name = "nombre")
+	private String nombre;
+	
+	@Column(name = "apellido")
+	private String apellido;
+	
+	@Column(name = "email")
+	private String email;
+	
+	@Column(name = "telefono")
+	private String telefono;
+	
+	@Column(name = "fecha_ingreso")
+	@Temporal(TemporalType.DATE)
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	private Date fecha;
+
+	public Empleado() {
+	}
+
+	public Empleado(Long id, String nombre, String apellido, String email, String telefono, Date fecha) {
+		this.id = id;
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.email = email;
+		this.telefono = telefono;
+		this.fecha = fecha;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+
+	public Date getFecha() {
+		return fecha;
+	}
+
+	public void setFecha(Date fecha) {
+		this.fecha = fecha;
+	}
+	
+	
+	
+	
+	
+	
+}
